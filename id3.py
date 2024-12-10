6c598e9bdaaba4c93779b1178184e87c0c04868d
vdamasceno04 <vdamasceno04@gmail.com>
2024-12-10 00:55:21 -0300

    aux functions

--- id3.py
+++ id3.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 from collections import Counter
 from sklearn.model_selection import KFold
+from aux import entropy, information_gain, best_split, median_split
 
 class Node:
     def __init__(self, attribute=None, threshold=None, left=None, right=None, label=None):
@@ -11,78 +12,6 @@ class Node:
         self.left = left
         self.right = right
         self.label = label
-
-def entropy(labels):
-    label_counts = Counter(labels)
-    total_labels = len(labels)
-    entropy_value = 0.0
-
-    for label_count in label_counts.values():
-        probability = label_count / total_labels
-        entropy_value -= probability * math.log2(probability)
-
-    return entropy_value
-
-def information_gain(parent_labels, left_labels, right_labels):
-    parent_entropy = entropy(parent_labels)
-    left_entropy = entropy(left_labels)
-    right_entropy = entropy(right_labels)
-    
-    left_weight = len(left_labels) / len(parent_labels)
-    right_weight = len(right_labels) / len(parent_labels)
-    
-    return parent_entropy - (left_weight * left_entropy + right_weight * right_entropy)
-
-def best_split(data, labels):
-    best_gain = 0
-    best_attribute = None
-    best_split_point = None
-    n_features = data.shape[1]
-
-    for feature in range(n_features):
-        unique_values = np.unique(data[:, feature])
-        for split_point in unique_values:
-            left_indices = data[:, feature] <= split_point
-            right_indices = data[:, feature] > split_point
-
-            left_labels = labels[left_indices]
-            right_labels = labels[right_indices]
-            
-            gain = information_gain(labels, left_labels, right_labels)
-            
-            if gain > best_gain:
-                best_gain = gain
-                best_attribute = feature
-                best_split_point = split_point
-
-    return best_attribute, best_split_point
-
-def median_split(data, labels):
-    best_gain = 0
-    best_attribute = None
-    best_split_point = None
-    n_features = data.shape[1]
-
-    for feature in range(n_features):
-        # Calcula a mediana do atributo
-        split_point = np.median(data[:, feature])
-
-        # Divide os dados com base na mediana
-        left_indices = data[:, feature] <= split_point
-        right_indices = data[:, feature] > split_point
-
-        left_labels = labels[left_indices]
-        right_labels = labels[right_indices]
-
-        # Calcula o ganho de informação para essa divisão
-        gain = information_gain(labels, left_labels, right_labels)
-
-        if gain > best_gain:
-            best_gain = gain
-            best_attribute = feature
-            best_split_point = split_point
-
-    return best_attribute, best_split_point
 
 def insert_tree(data, label, split):
     if len(set(label)) == 1: #se só há 1 rótulo, ele é raiz
